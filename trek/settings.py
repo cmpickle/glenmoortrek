fde1e0281c04da120684ddf7c1cecdd32fea7ebe
Cameron Pickle <cmpickle@gmail.com>
2018-01-15 09:13:24 -0700

    set debug to false

--- trek/settings.py
+++ trek/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'riz##!khrd0pg@v!eb+^no*$cz9)qlaj%oq$fi=u4vzl*k0a0r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
