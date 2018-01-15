bcbd58739cde09707e90dbd801e1605cc5572947
Cameron Pickle <cmpickle@gmail.com>
2018-01-15 09:16:42 -0700

    achievement.... not achievements

--- achievement/admin.py
+++ achievement/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from achievements.models import Achievement
+from achievement.models import Achievement
 
 admin.site.register(Achievement)
