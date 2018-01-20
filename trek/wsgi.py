03e377bc43df556a26499d7bb9a6db53ac1807b2
Cameron Pickle <cmpickle@gmail.com>
2018-01-19 19:50:52 -0700

    adding logging

--- trek/wsgi.py
+++ trek/wsgi.py
@@ -8,12 +8,16 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import logging
 
 from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trek.settings")
+
+logger = logging.getLogger('herokulogger')
+logger.info('Starting applicatoin')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
